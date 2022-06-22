a192b3230b4ca4fd6281a886dea206a2c09bdb79
Brianna Goodrum <brianna.goodrum@gmail.com>
2022-06-22 11:07:07 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -108,7 +108,7 @@ function loginFunction() {
     },
     {
     //Account Fields
-    "id":userID, //Required
+    "id":userId, //Required
     "name": firstName + " "+ lastName + "of Star Command"
     });
 }
