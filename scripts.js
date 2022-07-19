bb41b68f5058b7626b4fa2ed4c0a6a5617fa10d6
Brianna Goodrum <brianna.goodrum@gmail.com>
2022-07-19 11:42:11 -0500

    disabling aptrinsic
    testing for a ticket

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
-(function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
+/*(function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
     (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
   var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
   var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-})(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-M9A6K3DJSGSV-2");
+})(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-M9A6K3DJSGSV-2");*/
 
 //event listener for filter
 window.addEventListener('load', (event) => {
