4f75a5ceaa24d59e90a931002c9eb615b1a7513a
Brianna Goodrum <brianna.goodrum@gmail.com>
2022-09-12 13:14:45 -0500

    eliminate CORS error

--- scripts.js
+++ scripts.js
@@ -1,4 +1,23 @@
-(function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
+
+(function(n, t, a, e, x) {
+    let i = "aptrinsic";
+    n[i] = n[i] || function() {
+            (n[i].q = n[i].q || []).push(arguments)
+        },
+        n[i].p = e, n[i].c = x;
+
+    let r = t.createElement("script");
+    r.async = !0;
+    r.src = a + "?a=" + e;
+
+    let c = t.getElementsByTagName("script")[0];
+    c.parentNode.insertBefore(r, c);
+})
+(window, document, "https://web-sdk.aptrinsic.com/api/aptrinsic.js", "AP-M9A6K3DJSGSV-2", {
+    cssNonce: "r@nd0m"
+});
+
+/*(function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
     (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
   var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
   var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
@@ -6,7 +25,7 @@
     "htmlSanitization": true,
     "htmlSanitizationAllowedDomains": ["https://aptrinsic.com/*"],
     "engagementChecksumFileUrl": "https://https://br1b3.github.io/checksums.json"
-    };
+    };*/
 
 //event listener for filter
 window.addEventListener('load', (event) => {
