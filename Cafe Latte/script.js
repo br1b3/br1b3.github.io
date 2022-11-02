2920e4896f6383d03e53fa766f5c0a96e3732a7b
Brianna Goodrum <brianna.goodrum@gmail.com>
2022-11-02 12:02:27 -0500

    adding PX tag to Cafe Latte
    check to see if this is tracking

--- Cafe Latte/script.js
+++ Cafe Latte/script.js
@@ -1,5 +1,14 @@
 //aptrinsic call
+(function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
+    (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
+  var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
+  var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
+})(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-VCRMFF1YBD96-2");
 
+//global context
+aptrinsic('set', 'globalContext', {"sitename":"Cafe Latte"});
+
+//side menu trigger
 function sideMenuTriggerOn(item)  {
     item.style.visibility = 'visible'
 }
