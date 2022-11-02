7a11560b25c1197a92442eda3f42fd42a2239bbe
Brianna Goodrum <brianna.goodrum@gmail.com>
2022-11-02 13:57:16 -0500

    pretty sure this is wrong

--- hub-scripts.js
+++ hub-scripts.js
@@ -48,14 +48,24 @@ function loginFunction() {
         "name": user1.accountName
         });
         //User Preferences
-        aptrinsic(
-        {
-            "trackUsage": true,
-            "productUpdates": true,
-            "guidedTours": false,
-            "surveys": false,
-            "onboardingBot": false
+        put(url, data); {
+ 
+            // Awaiting fetch which contains method,
+            // headers and content-type and body
+            const response = await fetch('http://api.aptrinsic.com/v1/user/preferences/user1@starcommand.unv', {
+              method: 'PUT',
+              headers: {
+                'Content-type': 'application/json', 'X-APTRINSIC-API-KEY': '8e1cabeb-7131-447a-bd9b-632277771fec'
+              },
+              body: {
+                  "trackUsage": true,
+              "productUpdates": true,
+              "guidedTours": false,
+              "surveys": false,
+              "onboardingBot": false
+            }
         });
+        };
     }
     else if (username === user2.userId && password === user2.password){
         setTimeout(() => { window.location.href="home.html"; }, 500);
