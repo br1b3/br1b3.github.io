7ee3d13d6406206b0e7df722d1291d8a56fc8955
Brianna Goodrum <brianna.goodrum@gmail.com>
2022-11-02 20:26:57 -0500

    removed extra "s"

--- WowNews/script.js
+++ WowNews/script.js
@@ -1,9 +1,11 @@
+//aptrinsic call
 (function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
     (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
   var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
   var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
 })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-FLT1ANRKZSFY-2");
 
+//globalcontext and custom event
 var url = window.location.pathname;
 var article = url.substring(url.lastIndexOf('/')+1);
 
