dd73c68ad8e2720b7e3e09977df414c84d100fa2
Brianna Goodrum <brianna.goodrum@gmail.com>
2023-03-20 15:25:49 -0500

    changed tag

--- CafeLatte/script.js
+++ CafeLatte/script.js
@@ -3,16 +3,9 @@
     (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
   var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
   var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-})(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-VCRMFF1YBD96-2");
+})(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-HT89MSFYKI6R-2");
 
 console.log(aptrinsic.init);
-
-//aptrinsic call
-/*(function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
-    (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
-  var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
-  var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-})(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-VCRMFF1YBD96-2");*/
 
 //global context
 aptrinsic('set', 'globalContext', {"sitename":"Cafe Latte"});
