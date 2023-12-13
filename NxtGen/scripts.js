9f3d6d03f4ec221d99db7ca00187dbdc42d1841f
Brianna Goodrum <brianna.goodrum@gmail.com>
2023-12-13 13:53:37 -0600

    changing PX Tag on iframe

--- NxtGen/scripts.js
+++ NxtGen/scripts.js
@@ -4,7 +4,7 @@
     (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
   var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
   var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-})(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-M9A6K3DJSGSV-2", {localStorageCookie: true}), {
+})(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-M9A6K3DJSGSV-2", {localStorageCookie: true}, { "iframeModeEnabled": false }), {
     "htmlSanitization": true,
     "htmlSanitizationAllowedDomains": ["https://aptrinsic.com/*"],
     "engagementChecksumFileUrl": "https://https://br1b3.github.io/checksums.json"
